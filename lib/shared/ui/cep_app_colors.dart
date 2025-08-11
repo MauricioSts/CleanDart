55ccc446929461b8d4d3f1b5e8b7be3129993121
MauricioSts <mauricio.santos.110@ufrn.edu.br>
2025-08-11 15:22:34 -0300

    Criação do aplicativo com o intuito de estudar clean dart

---
+++ lib/shared/ui/cep_app_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+sealed class CepAppColors{
+  static  const Color whiteColor = Color(0xffffffff);
+  static const Color blackColor = Color(0xff000000);
+  static const Color primaryColor = Color (0xffe040fb);
+  static const Color secondaryColor = whiteColor;
+  static const Color errorColor = Color(0XFFFF5252);
+
+
+  //DARKT THEME
+
+  static const Color darkBgColor = Color(0xff222222);
+
+  
+  //lightheme
+  static const Color lightBgColor = whiteColor;
+
+  
+
+}
