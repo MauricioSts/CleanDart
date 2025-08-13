369dcb95c8e1a83c0e69a01bee77a2ed73c350e3
MauricioSts <mauricio.santos.110@ufrn.edu.br>
2025-08-13 15:36:01 -0300

    criação do sharedPreferences, apiProvider e localProvider

--- lib/shared/data/local/local_service/local_service.dart
+++ lib/shared/data/local/local_service/local_service.dart
@@ -2,7 +2,7 @@ import 'package:cep_app/shared/data/async/either.dart';
 import 'package:cep_app/shared/data/local/local_service/errors/local_exception.dart';
 
 abstract interface class LocalService {
-  Future<Either<LocalException, T>> get<T>(String key);
+  Future<Either<LocalException, T?>> get<T>(String key);
 
   Future<Either<LocalException, void>> set<T>(String key, T data);
 }
---
+++ lib/shared/data/local/local_service/shared_preferences_service.dart
@@ -0,0 +1,55 @@
+import 'dart:developer';
+
+import 'package:cep_app/shared/data/async/either.dart';
+import 'package:cep_app/shared/data/local/local_service/errors/local_exception.dart';
+import 'package:cep_app/shared/data/local/local_service/local_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final class SharedPreferencesService implements LocalService {
+  @override
+  Future<Either<LocalException, T?>> get<T>(String key) async {
+    try {
+      final instance = await SharedPreferences.getInstance();
+
+      if (T == String) {
+        return Right(instance.getString(key) as T?);
+      } else if (T == bool) {
+        return Right(instance.getBool(key) as T?);
+      }
+      return Right(null);
+    } catch (error, st) {
+      log('error loading local string or bool', error: error, stackTrace: st);
+      return Left(
+        const LocalException(
+          message: 'error loading cache. please try again later',
+        ),
+      );
+    }
+  }
+
+  @override
+  Future<Either<LocalException, void>> set<T>(String key, T data) async {
+    try {
+      final instance = await SharedPreferences.getInstance();
+      if (T == String) {
+        await instance.setString(key, data as String);
+      } else if (T == bool) {
+        await instance.setBool(key, data as bool);
+      }
+
+      return Right(null);
+    } catch (error, st) {
+      log(
+        'error setting local string or bool ',
+        error: error,
+        stackTrace: st,
+      );
+    }
+    ;
+    return Left(
+      const LocalException(
+        message: "error setting cache. please try again later",
+      ),
+    );
+  }
+}
