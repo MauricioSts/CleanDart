c09a9306ef75074ac09ce04919bfef2667dcc296
MauricioSts <mauricio.santos.110@ufrn.edu.br>
2025-08-19 15:32:58 -0300

    criacao do themenotifierprovider

--- lib/shared/ui/theme/data/datasources/providers/theme_notifier_provider.dart
+++ lib/shared/ui/theme/data/datasources/providers/theme_notifier_provider.dart
@@ -1,9 +1,23 @@
+import 'package:cep_app/shared/data/local/local_service/shared_preferences_service.dart';
+import 'package:cep_app/shared/ui/theme/data/datasources/get_theme_local_datasource.dart';
 import 'package:cep_app/shared/ui/theme/data/datasources/providers/theme_notifier.dart';
 import 'package:cep_app/shared/ui/theme/data/datasources/providers/theme_state.dart';
-import 'package:cep_app/shared/ui/theme/domain/repositories/theme_repository.dart';
+import 'package:cep_app/shared/ui/theme/data/datasources/set_theme_local_datasource.dart';
+import 'package:cep_app/shared/ui/theme/data/repositories/theme_repository_impl.dart';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final ThemeNotifierProvider = StateNotifierProvider<ThemeNotifier, ThemeState>((ref){
-  return ThemeNotifier(ThemeRepositoryImpl)
-});
-
+final ThemeNotifierProvider = StateNotifierProvider<ThemeNotifier, ThemeState>(
+  (ref) {
+    return ThemeNotifier(
+      ThemeRepositoryImpl(
+        GetThemeLocalDatasourceImpl(
+          SharedPreferencesService(),
+        ),
+        SetThemeLocalDatasourceImpl(
+          SharedPreferencesService(),
+        ),
+      ),
+    );
+  },
+);
